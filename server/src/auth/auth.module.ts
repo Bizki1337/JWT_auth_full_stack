66664b27357477d732441630444952907c370a4c
Соловьёв Николай <bizki@MacBook-Pro-Solovev.local>
2023-11-04 12:29:33 +0300

    added jwt module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { AtStrategy, RtStrategy } from './strategies'
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService, RtStrategy, AtStrategy]
+	imports: [JwtModule.register({})],
+	controllers: [AuthController],
+	providers: [AuthService, RtStrategy, AtStrategy]
 })
 export class AuthModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt'
 
 import { PrismaService } from 'src/prisma/prisma.service'
@@ -7,22 +8,55 @@ import { Tokens } from './types'
 
 @Injectable()
 export class AuthService {
-	constructor(private prisma: PrismaService) {}
+	constructor(
+		private prisma: PrismaService,
+		private jwtService: JwtService 
+	) {}
 
 	hashData(data: string) {
 		return bcrypt.hash(data, 10)
 	}
 
+	async getTokens(userId: number, login: string): Promise<Tokens> {
+		const [at, rt] = await Promise.all([
+			this.jwtService.signAsync({
+				sub: userId,
+				login
+			}, {
+				secret: 'at-secret',
+				// acces token will live 15 minutes
+				expiresIn: 60 * 15
+			}),
+			this.jwtService.signAsync({
+				sub: userId,
+				login
+			}, {
+				secret: 'rt-secret',
+				// refresh token will live 7 days
+				expiresIn: 60 * 60 * 24 * 7
+			})
+		])
+
+		return {
+			access_token: at,
+			refresh_token: rt,
+		}
+	}
+
 	async signupLocal(dto: AuthDto): Promise<Tokens> {
 		const { login, password, name } = dto
 		const hash = await this.hashData(password)
-		const newUser = this.prisma.user.create({
+		const newUser = await this.prisma.user.create({
 			data: {
 				login,
 				name,
 				hash
 			}
 		})
+
+		const tokens = await this.getTokens(newUser.id, newUser.login)
+
+		return tokens
 	}
 
 	signinLocal() {}
