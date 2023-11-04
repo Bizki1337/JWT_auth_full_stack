599252a8c18ab49d8ded6509bf5a8ff31651bf73
Соловьёв Николай <bizki@MacBook-Pro-Solovev.local>
2023-11-04 16:41:15 +0300

    added logout function

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
 
 import { AuthService } from './auth.service'
 import { AuthDto, SigninDto } from './dto'
@@ -8,23 +8,25 @@ import { Tokens } from './types'
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
-	@Post('/local/signup')
+	@Post('local/signup')
+	@HttpCode(HttpStatus.CREATED)
 	signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
 		return this.authService.signupLocal(dto)
 	}
 
-	@Post('/local/signin')
+	@Post('local/signin')
+	@HttpCode(HttpStatus.OK)
 	signinLocal(@Body() dto: SigninDto): Promise<Tokens> {
 		return this.authService.signinLocal(dto)
 	}
 
-	@Post('/logout')
+	@Post('logout')
 	logout() {
-		return this.authService.logout()
+		// return this.authService.logout()
 	}
 
-	@Post('/refresh')
+	@Post('refresh')
 	refreshTokens() {
-		return this.authService.refreshTokens()
+		// return this.authService.refreshTokens()
 	}
 }
